6549bd7a4d8b991bff5b0a9c1f04f87aaf6f40b8
JorgeCiro <jorge.r.ciro@gmail.com>
2024-12-10 16:08:46 +0100

    Update Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -1,1 +1,14 @@
+public class Saludo {
+    public static void saludoInicial() {
+        System.out.println("¡Hola, bienvenido!.");
+    }
 
+    public static void despedida() {
+        System.out.println("¡Hasta luego!");
+    }
+
+    public static void main(String[] args) {
+        saludoInicial();
+        despedida();
+    }
+}
